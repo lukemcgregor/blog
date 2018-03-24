72f8e52027f45c6fff20bfbddff7afd2898f3b14
Luke McGregor <lukemcgregor@users.noreply.github.com>
2018-03-25 00:32:17 +1300

    Doing it cli

--- script/cibuild.sh
+++ script/cibuild.sh
@@ -2,4 +2,4 @@
 set -e # halt script on error
 
 bundle exec jekyll build
-bundle exec ./script/test.rb
+bundle exec htmlproofer --href-ignore "/thomascoats.com/,/dotdev.io/,/seofriendlysinglepageapp.azurewebsites.net/" ./_site
