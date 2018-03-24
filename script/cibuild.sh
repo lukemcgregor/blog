193faad31688a1c284029e358f426f8618db9849
Luke McGregor <lukemcgregor@users.noreply.github.com>
2018-03-25 00:42:01 +1300

    trying no externals

--- script/cibuild.sh
+++ script/cibuild.sh
@@ -2,4 +2,4 @@
 set -e # halt script on error
 
 bundle exec jekyll build
-bundle exec htmlproofer --url-ignore "/thomascoats.com/,/dotdev.io/,/seofriendlysinglepageapp.azurewebsites.net/" ./_site
+bundle exec htmlproofer --url-ignore "#" --disable_external ./_site
