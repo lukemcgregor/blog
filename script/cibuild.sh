d955bf3b6e024eeb499f116abfbb0b4061b20d19
Luke McGregor <lukemcgregor@users.noreply.github.com>
2018-06-24 14:05:36 +1200

    undo...

--- script/cibuild.sh
+++ script/cibuild.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-export JEKYLL_ENV=production
-export NOKOGIRI_USE_SYSTEM_LIBRARIES=true
-
 bundle exec jekyll build
 bundle exec htmlproofer --disable-external --allow-hash-href --url-ignore "/#/" ./_site
